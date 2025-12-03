e95f734fbe9e5d4184b334852b49f3c3bd5d73ec
Gyanesh Patel <gyaneshpatel605@gmail.com>
2025-12-03 22:17:42 +0530

    Update upstream repository URL in config.py

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/toxicpapa00/ShrutixMusic",
+    "https://github.com/noxxop/ShrutixMusic",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
